88cbc957826527f70fd816e963f2fd776e3f6b4c
Timothy Andrew <mail@timothyandrew.net>
2013-08-19 16:57:14 +0530

    A wiki's subdomain cannot have spaces or special characters.

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,7 +1,10 @@
 class Wiki < ActiveRecord::Base
-  validates_uniqueness_of :subdomain, case_sensitive: false
   scope :case_insensitive_find_by_subdomain, lambda {|subdomain| where("lower(subdomain) = ?", subdomain.downcase) }
+
   has_many :users
-  validates_associated :users
   accepts_nested_attributes_for :users
+
+  validates_uniqueness_of :subdomain, case_sensitive: false
+  validates_associated :users
+  validates_format_of :subdomain, with: /\A\w+\z/, message: I18n.t("activerecord.errors.messages.subdomain_format")
 end
--- spec/models/wiki_spec.rb
+++ spec/models/wiki_spec.rb
@@ -14,4 +14,23 @@ describe Wiki do
       end
     end
   end
+
+  context "validations" do
+    context "when validating the subdomain" do
+      it "allows a subdomain with only alphanumeric characters" do
+        wiki = build(:wiki, subdomain: "Alphanumer1c")
+        wiki.should be_valid
+      end
+
+      it "doesn't allow a subdomain with special characters" do
+        wiki = build(:wiki, subdomain: "Spec!@l")
+        wiki.should_not be_valid
+      end
+
+      it "doesn't allow a subdomain with spaces" do
+        wiki = build(:wiki, subdomain: "Wiki with Spaces")
+        wiki.should_not be_valid
+      end
+    end
+  end
 end
