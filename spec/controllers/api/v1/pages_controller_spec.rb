1e6eff36873144a566a6ae6d2cec9c64444e69d6
Timothy Andrew <mail@timothyandrew.net>
2013-08-20 23:36:18 +0530

    [#55495324] Use the `module Api` syntax instead of `API::V1` for the PagesController spec.

--- spec/controllers/api/v1/pages_controller_spec.rb
+++ spec/controllers/api/v1/pages_controller_spec.rb
@@ -1,52 +1,55 @@
 require 'spec_helper'
+module Api
+  module V1
+    describe PagesController do
+      context "PUT 'update'" do
 
-describe Api::V1::PagesController do
-  context "PUT 'update'" do
+        context "when the save is successful" do
+          it "returns http success" do
+            page = create(:page, title: "Bar")
+            put :update, id: page.id, page: {title: "Foo"}
+            response.should be_success
+          end
 
-    context "when the save is successful" do
-      it "returns http success" do
-        page = create(:page, title: "Bar")
-        put :update, id: page.id, page: {title: "Foo"}
-        response.should be_success
+          it "updates the page corresponding to the passed ID" do
+            page = create(:page, title: "Bar")
+            put :update, id: page.id, page: {title: "Foo"}
+            page.reload.title.should == "Foo"
+          end
+
+          it "returns the page in JSON" do
+            page = create(:page, title: "Bar", text: "Baz")
+            put :update, id: page.id, page: {title: "Foo"}
+            response_hash = JSON.parse(response.body).symbolize_keys
+            response_hash.slice(:text, :title).should == {text: "Baz", title: "Foo"}
+          end
+        end
+
+        context "if the save fails due to a validation error" do
+          it "returns a 400 if the save fails due to a validation error" do
+            page = create(:page)
+            put :update, id: page.id, page: {title: nil}
+            response.status.should == 400
+          end
+
+          it "doesn't update the page" do
+            page = create(:page, text: "Foo")
+            put :update, id: page.id, page: {title: nil, text: "Bar"}
+            page.reload.text.should == "Foo"
+          end
+
+          it "returns the errors in JSON" do
+            page = create(:page, title: "Bar", text: "Baz")
+            put :update, id: page.id, page: {title: nil}
+            response_hash = JSON.parse(response.body).symbolize_keys
+            response_hash.slice(:title).should == {title: ["can't be blank"]}
+          end
+        end
+
+        it "raises an error if no ID is passed" do
+          expect { put :update, page: {title: "Foo"} }.to raise_error
+        end
       end
-
-      it "updates the page corresponding to the passed ID" do
-        page = create(:page, title: "Bar")
-        put :update, id: page.id, page: {title: "Foo"}
-        page.reload.title.should == "Foo"
-      end
-
-      it "returns the page in JSON" do
-        page = create(:page, title: "Bar", text: "Baz")
-        put :update, id: page.id, page: {title: "Foo"}
-        response_hash = JSON.parse(response.body).symbolize_keys
-        response_hash.slice(:text, :title).should == { text: "Baz", title: "Foo" }
-      end
-    end
-
-    context "if the save fails due to a validation error" do
-      it "returns a 400 if the save fails due to a validation error" do
-        page = create(:page)
-        put :update, id: page.id, page: {title: nil}
-        response.status.should == 400
-      end
-
-      it "doesn't update the page" do
-        page = create(:page, text: "Foo")
-        put :update, id: page.id, page: {title: nil, text: "Bar"}
-        page.reload.text.should == "Foo"
-      end
-
-      it "returns the errors in JSON" do
-        page = create(:page, title: "Bar", text: "Baz")
-        put :update, id: page.id, page: { title: nil }
-        response_hash = JSON.parse(response.body).symbolize_keys
-        response_hash.slice(:title).should == { title: ["can't be blank"] }
-      end
-    end
-
-    it "raises an error if no ID is passed" do
-      expect { put :update, page: { title: "Foo" } }.to raise_error
     end
   end
 end
