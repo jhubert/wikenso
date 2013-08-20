9f3ec77b716b689886ab2af57a9e3cecb9ad74a7
Timothy Andrew <mail@timothyandrew.net>
2013-08-20 18:33:08 +0530

    [#55465876] Use 'module API' syntax instead of API::V1

--- app/controllers/api/V1/pages_controller.rb
+++ app/controllers/api/V1/pages_controller.rb
@@ -1,17 +1,21 @@
-class Api::V1::PagesController < ApplicationController
-  def update
-    page = Page.find(params[:id])
-    if page.update(page_params)
-      render json: page
-    else
-      render json: page.errors, status: :bad_request
+module Api
+  module V1
+    class PagesController < ApplicationController
+      def update
+        page = Page.find(params[:id])
+        if page.update(page_params)
+          render json: page
+        else
+          render json: page.errors, status: :bad_request
+        end
+
+      end
+
+      private
+
+      def page_params
+        params.require(:page).permit(:text, :title)
+      end
     end
-
-  end
-
-  private
-
-  def page_params
-    params.require(:page).permit(:text, :title)
   end
 end
