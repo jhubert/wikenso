93bbd227fbf761b0d5196270e58c6e321d9b63f2
Timothy Andrew <mail@timothyandrew.net>
2013-08-21 16:11:21 +0530

    Try moving to other module syntax

--- app/controllers/api/V1/pages_controller.rb
+++ app/controllers/api/V1/pages_controller.rb
@@ -1,21 +1,17 @@
-module Api
-  module V1
-    class PagesController < ApplicationController
-      def update
-        page = Page.find(params[:id])
-        if page.update(page_params)
-          render json: {}
-        else
-          render json: page.errors, status: :bad_request
-        end
-
-      end
-
-      private
-
-      def page_params
-        params.require(:page).permit(:text, :title)
-      end
+class Api::V1::PagesController < ApplicationController
+  def update
+    page = Page.find(params[:id])
+    if page.update(page_params)
+      render json: {}
+    else
+      render json: page.errors, status: :bad_request
     end
+
+  end
+
+  private
+
+  def page_params
+    params.require(:page).permit(:text, :title)
   end
 end
