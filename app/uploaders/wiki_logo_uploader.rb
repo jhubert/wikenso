6fc5bbbd3c0c151f4bc49c81875ef67fa61eebdf
Timothy Andrew <mail@timothyandrew.net>
2013-08-30 14:52:18 +0530

    [#55466150] Add a default wiki logo.

--- app/uploaders/wiki_logo_uploader.rb
+++ app/uploaders/wiki_logo_uploader.rb
@@ -5,13 +5,9 @@ class WikiLogoUploader < CarrierWave::Uploader::Base
     "uploads/#{model.subdomain.downcase}/logo"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
+  def default_url
+     "/assets/default_logo.png"
+  end
 
   def extension_white_list
      %w(jpg jpeg png)
